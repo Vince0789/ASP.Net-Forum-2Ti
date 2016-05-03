b2ef99b39e07d4af9c79d18f853673b092500510
Vince Vandormael <r0254260@student.thomasmore.be>
2016-05-03 17:12:33 +0200

    blforum

---
+++ App_Code/BLForum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BLForum
+/// </summary>
+public class BLForum
+{
+    protected AspLinqDataContext dc;
+
+    public BLForum()
+    {
+        dc = new AspLinqDataContext();
+    }
+
+    public Forum GetForumById(int id)
+    {
+        return (from Forum in dc.Forums where Forum.Id == id select Forum).SingleOrDefault();
+    }
+
+    public List<Forum> GetParentForums()
+    {
+        AspLinqDataContext dc = new AspLinqDataContext();
+        return (from Forum in dc.Forums where Forum.ParentForumId == null select Forum).ToList();
+    }
+}
