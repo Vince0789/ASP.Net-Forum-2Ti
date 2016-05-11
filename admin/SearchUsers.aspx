89d0abffe42749673447e7dfd7d2c3aa99a2ebc5
Vince Vandormael <r0254260@student.thomasmore.be>
2016-05-11 11:16:25 +0200

    search users

--- admin/SearchUsers.aspx
+++ admin/SearchUsers.aspx
@@ -2,5 +2,12 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderBody" Runat="Server">
 	<h2>Results for "<asp:Label ID="LabelQuery" runat="server" Text="Label"></asp:Label>"</h2>
+  <asp:GridView ID="GridViewSearchResults" runat="server" AllowPaging="True" AutoGenerateColumns="False" CssClass="table" CellSpacing="-1" GridLines="None" OnPageIndexChanging="GridViewSearchResults_PageIndexChanging">
+    <Columns>
+      <asp:BoundField DataField="Name" HeaderText="Username" />
+      <asp:BoundField DataField="RegistrationDate" HeaderText="Member since" DataFormatString="{0:dd MMM yyyy}" />
+    </Columns>
+    <PagerStyle CssClass="pagination-ys" />
+  </asp:GridView>
 </asp:Content>
 
--- admin/SearchUsers.aspx.cs
+++ admin/SearchUsers.aspx.cs
@@ -9,11 +9,28 @@ public partial class admin_SearchUsers : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
-		string query = Request.QueryString["query"];
-
-		if (string.IsNullOrWhiteSpace(query))
-			throw new HttpException(400, "Bad Request");
-
-		LabelQuery.Text = query;
+        PopulateGridView();
 	}
+
+    protected void GridViewSearchResults_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridViewSearchResults.PageIndex = e.NewPageIndex;
+        PopulateGridView();
+    }
+
+    protected void PopulateGridView()
+    {
+        string query = Request.QueryString["query"];
+
+        if (string.IsNullOrWhiteSpace(query))
+            throw new HttpException(400, "Bad Request");
+
+        LabelQuery.Text = query;
+
+        BLMember blMember = new BLMember();
+        List<Member> results = blMember.FindMembersByName(query).OrderBy(m => m.Name).ToList();
+
+        GridViewSearchResults.DataSource = results;
+        GridViewSearchResults.DataBind();
+    }
 }
